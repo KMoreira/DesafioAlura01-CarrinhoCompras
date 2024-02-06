3edb53ec669bea7f90b7bf2955f758f7a4485d22
Kainã Moreira <78170911+KMoreira@users.noreply.github.com>
2024-02-06 20:52:39 +0000

    Desafio Concluido

--- js/app.js
+++ js/app.js
@@ -1,7 +1,30 @@
-function adicionar(){
-    alert('botão adicionar');
-}
+let totalGeral = 0;
+document.getElementById('valor-total').textContent = '0';
+document.getElementById('lista-produtos').innerHTML = '';
 
+function adicionar(){
+  let produto = document.getElementById('produto').value;
+  let nomeProduto = produto.split('-')[0];
+  let valorProduto = produto.split('R$')[1];
+  let qtd = document.getElementById('quantidade').value;
+
+  document.getElementById('quantidade').value = 0;
+
+  let preco = qtd * valorProduto;
+
+  let carrinho = document.getElementById('lista-produtos');
+  carrinho.innerHTML = carrinho.innerHTML + `        <section class="carrinho__produtos__produto">
+  <span class="texto-azul">${qtd}x</span>${nomeProduto}<span class="texto-azul">R$${preco}</span>
+  </section>`;
+  
+  totalGeral = totalGeral + preco;
+  let campoTotal = document.getElementById('valor-total');
+  campoTotal.textContent = `R$ ${totalGeral}`;
+  
+
+}
 function limpar(){
-    alert('botão limpar');
+  totalGeral = 0;
+  document.getElementById('valor-total').textContent = '0';
+  document.getElementById('lista-produtos').innerHTML = '';
 }
